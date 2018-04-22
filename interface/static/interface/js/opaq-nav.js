b71352fb8cacafb916a378567067b503489b6418
Mohit Negi <mohitnegi165@gmail.com>
2018-04-22 20:09:58 +0530

    Delete opaq-nav.js

--- interface/static/interface/js/opaq-nav.js
+++
@@ -1,9 +0,0 @@
-
-    $(window).scroll(function() {
-	    if($(this).scrollTop() > 500) {
-	        $('.navbar').addClass('opaq-nav');
-	    }
-	    else {
-	        $('.navbar').removeClass('opaq-nav');
-	    }
-	});
