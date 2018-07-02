001863fed5e1a5ab0424ed2bd05be3fe54a2e77b
mohit <mohitnegi165@gmail.com>
2018-07-03 00:39:31 +0530

    cleanup

--- routing.py
+++
@@ -1,9 +0,0 @@
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
-import interface.routing
-
-application = ProtocolTypeRouter({
-    'websocket': AuthMiddlewareStack(
-        URLRouter(interface.routing.ws_urlpatterns)
-    )
-})
