748a270e0e29bb889c72c4805d83e7b0d17ac75f
mohit <mohitnegi165@gmail.com>
2018-07-06 16:49:11 +0530

    consumer for chat messages

--- interface/consumers.py
+++ interface/consumers.py
@@ -4,7 +4,7 @@ import json
 
 # import logging
 
-# logging.basicConfig(filename='/home/mohit/Documents/git_repos/chat-con/log.txt',
+# logging.basicConfig(filename='/home/mohit/Documents/git_repos/chat-con/debug.log',
 #                     level=logging.DEBUG,
 #                     filemode='w')
 # logger = logging.getLogger()
@@ -18,8 +18,6 @@ class ChatConsumer(AsyncWebsocketConsumer):
         self.user_obj = ''
 
     async def connect(self):
-        # logger.debug('000000000>>>>>>>>> connected')
-        # self.room_name = 'online'
         self.room_group_name = 'online_users'
 
         await self.channel_layer.group_add(
@@ -29,7 +27,6 @@ class ChatConsumer(AsyncWebsocketConsumer):
         await self.accept()
 
     async def disconnect(self, close_code):
-        # logger.debug('000000000>>>>>>>>> diconnected')
         self.user_obj.is_online = False
         self.user_obj.save()
         await self.channel_layer.group_send(
@@ -43,26 +40,41 @@ class ChatConsumer(AsyncWebsocketConsumer):
             self.channel_name)
 
     async def receive(self, text_data):
-        # logger.debug('000000000>>>>>>>>> received')
-        user = json.loads(text_data)
-        usr_on = user['user_online']
-        
-        self.user_obj = models.User.objects.get(username__exact=usr_on)
-        self.user_obj.is_online = True
-        self.user_obj.save()
+        rec_msg = json.loads(text_data)
+        try:
+            usr_on = rec_msg['user_online']
+            full_name = rec_msg['full_name']
+            
+            self.user_obj = models.User.objects.get(username__exact=usr_on)
+            self.user_obj.is_online = True
+            self.user_obj.save()
 
-        await self.channel_layer.group_send(
-            self.room_group_name, {
-                'type': 'is_online',
-                'message': usr_on
-            })
+            await self.channel_layer.group_send(
+                self.room_group_name, {
+                    'type': 'is_online',
+                    'message': usr_on,
+                    'fulname': full_name
+                })
+        except KeyError:
+            msg_frm = rec_msg['from']
+            msg_to = rec_msg['to']
+            msg_txt = rec_msg['text']
+
+            await self.channel_layer.group_send(
+                self.room_group_name, {
+                    'type': 'text_msg',
+                    'frm': msg_frm,
+                    'to': msg_to,
+                    'txt': msg_txt
+                })
 
     async def is_online(self, event):
-        # logger.debug('000000000>>>>>>>>> is_online')
         usr = event['message']
+        full_name = event['fulname']
 
         await self.send(text_data=json.dumps({
                 'uname': usr,
+                'full_name': full_name,
                 'online': True
             }))
 
@@ -72,4 +84,15 @@ class ChatConsumer(AsyncWebsocketConsumer):
         await self.send(text_data=json.dumps({
                 'uname': usr,
                 'online': False
+            }))
+
+    async def text_msg(self, event):
+        frm = event['frm']
+        to = event['to']
+        txt = event['txt']
+
+        await self.send(text_data=json.dumps({
+                'from': frm,
+                'to': to,
+                'txt_msg': txt
             }))
