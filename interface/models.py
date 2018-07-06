e652ba8b30a84c1bd22b8a97b8eb1338f4971ff9
mohit <mohitnegi165@gmail.com>
2018-07-06 17:48:20 +0530

    set default first n last name

--- interface/models.py
+++ interface/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 
 class User(models.Model):
-    firstname = models.CharField(max_length=50)
-    lastname = models.CharField(max_length=70)
+    firstname = models.CharField(max_length=50, default='none')
+    lastname = models.CharField(max_length=70, default=' ')
     username = models.CharField(max_length=32, primary_key=True)
     phn_numb = models.CharField(max_length=13, unique=True)
     password = models.CharField(max_length=100)
