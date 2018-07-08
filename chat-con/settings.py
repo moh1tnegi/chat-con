448521657d2ca6ed155418034bdd346f9242aad2
Mohit Negi <mohitnegi165@gmail.com>
2018-07-08 08:33:07 +0530

    Update settings.py

--- chat-con/settings.py
+++ chat-con/settings.py
@@ -91,7 +91,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'staticfiles'),
 )
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
