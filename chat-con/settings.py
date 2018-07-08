e300bebf10fd913d9ec8c4866432788e5d92ce6f
Mohit Negi <mohitnegi165@gmail.com>
2018-07-09 01:17:36 +0530

    Update settings.py

--- chat-con/settings.py
+++ chat-con/settings.py
@@ -89,10 +89,11 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'staticfiles'),
 )
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
