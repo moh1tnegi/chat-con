ec8a31a950d9c49e4a0bb5d0a377d67fa2737085
Mohit Negi <mohitnegi165@gmail.com>
2018-07-09 00:07:52 +0530

    Update settings.py

--- chat-con/settings.py
+++ chat-con/settings.py
@@ -95,7 +95,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 LOGIN_REDIRECT_URL = '/'
 
